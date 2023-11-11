bbcd8f61c9c615644ac79e4a975543b1003cb415
Onur Sevket Aslan <osevas@gmail.com>
2023-11-12 00:04:16 +0300

    to initialize random action to states.  consider current car number

---
+++ chapter4/jack_car_rental.py
@@ -0,0 +1,82 @@
+'''
+Author: Onur Aslan
+Date: 2023-11-11
+Objective: coding example 4.2, Jack's Car Rental
+'''
+import numpy as np
+from scipy.stats import poisson
+
+class Shop:
+    """
+    object for shops
+    """
+    def __init__(self, num_cars, prob_rent, prob_return) -> None:
+        self.num_cars = num_cars
+        self.prob_rent = prob_rent
+        self.prob_return = prob_return
+        self.max_num_cars = 20
+        self.max_transfer = 5
+    
+    def poisson(self) -> int:
+        """
+        function for Poisson distribution
+
+        Args:
+            param_lambda (int): lambda in Poisson's distribution
+
+        Returns:
+            int: Poisson probability
+        """
+        return None
+
+class State:
+    """
+    object for state
+    """
+    def __init__(self, num_car_a, a_rent_lambda, a_return_lambda, num_car_b, b_rent_lambda, b_return_lambda) -> None:
+        self.shop_a = Shop(num_car_a, a_rent_lambda, a_return_lambda)
+        self.shop_b = Shop(num_car_b, b_rent_lambda, b_return_lambda)
+        self.val = 0
+
+
+class Environment:
+    """
+    object for environment
+    """
+    def __init__(self, n_grid=21) -> None:
+        self.state_list = []
+        for i in range(n_grid):
+            for j in range(n_grid):
+                self.state_list.append(State(i, 3, 3, j, 4, 2))
+        self.state_arr = np.array(self.state_list).reshape(n_grid, n_grid)
+        print(self.state_arr[-1, -1].shop_a.num_cars)
+        print(self.state_arr[-1, -1].shop_b.num_cars)
+        pass
+
+class Agent:
+    """
+    object for agent
+    """
+    def __init__(self) -> None:
+        self.actions = list(range(-5, 6, 1))
+    
+    @staticmethod
+    def print_actions():
+        return self.actions
+
+def main():
+    """
+    main function of the code
+
+    Returns:
+        _type_: _description_
+    """
+
+    # Initialization
+    agent1 = Agent()
+    env1 = Environment()
+    
+    return None
+
+if __name__ == "__main__":
+    main()
