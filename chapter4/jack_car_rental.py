47abb6a851402727d4f6be24dc155579aaf3ea04
Onur Sevket Aslan <osevas@gmail.com>
2023-11-13 18:30:16 +0300

    coded policy evaluation

--- chapter4/jack_car_rental.py
+++ chapter4/jack_car_rental.py
@@ -10,6 +10,10 @@ from scipy.stats import poisson
 actions = list(range(-5, 6, 1))
 MAX_NUM_CARS = 20
 MAX_TRANSFER = 5
+RENT_REWARD = 10
+TRANSFER_REWARD = -2
+GAMMA = 0.9
+THETA = 
 
 class Shop:
     """
@@ -90,6 +94,35 @@ class Agent:
     def __init__(self) -> None:
         self.actions = actions
 
+def calc_sigma(rent_a, state_array, rent_b, return_a, return_b, i, j):
+    """
+    calculating summation
+    """
+    if (rent_a[0] <= state_array[i, j].shop_a.num_cars) and (rent_b[0] <= state_array[i, j].shop_b.num_cars): 
+        # rentable car count should be less or equal to current car count in the shop
+        
+        # calculating shop's new car counts
+        if state_array[i, j].policy < 0:
+            # shop_b is transferring cars to shop_a
+            new_shop_a_num_car = state_array[i, j].shop_a.num_cars - rent_a[0] + return_a[0] + abs(state_array[i, j].policy)
+            new_shop_b_num_car = state_array[i, j].shop_b.num_cars - rent_b[0] + return_b[0] - abs(state_array[i, j].policy)
+        
+        elif state_array[i, j].policy > 0:
+            # shop_b is transferring cars to shop_a
+            new_shop_a_num_car = state_array[i, j].shop_a.num_cars - rent_a[0] + return_a[0] - abs(state_array[i, j].policy)
+            new_shop_b_num_car = state_array[i, j].shop_b.num_cars - rent_b[0] + return_b[0] + abs(state_array[i, j].policy)
+        
+        # checking if new car counts are within the limits
+        if (new_shop_a_num_car > 0) and (new_shop_b_num_car > 0):
+            # calculating probability of the state
+            total_prob = rent_a[1] * return_a[1] * rent_b[1] * return_b[1]
+
+            # calculating reward
+            reward = (rent_a[0] + rent_b[0]) * RENT_REWARD + state_array[i, j].policy * TRANSFER_REWARD
+
+            state_new_value += total_prob * (reward + GAMMA * state_array[new_shop_a_num_car, new_shop_b_num_car].val)
+    return state_new_value
+
 def policy_eval(state_array):
     """
     Policy evaluation in state array
@@ -101,10 +134,38 @@ def policy_eval(state_array):
         _type_: _description_
     """
     delta = 0
-    # looping over states in Environment.state_arr
-    for i in range(state_array.shape[0]):
-        for j in range(state_array.shape[1]):
-            print(f'# of cars in State {i},{j}: {state_array[i, j].shop_a.num_cars}')
+
+    while delta < THETA:
+        # looping over states in Environment.state_arr
+        for i in range(state_array.shape[0]):
+            for j in range(state_array.shape[1]):
+                # print(f'# of cars in State {i},{j}: {state_array[i, j].shop_a.num_cars}')
+                state_value_temp = state_array[i, j].val # recording initial state value
+
+                rent_prob_a, return_prob_a = state_array[i, j].shop_a.poisson()
+                rent_prob_b, return_prob_b = state_array[i, j].shop_b.poisson()
+
+                state_new_value = 0
+                # Looping over rent and return probabilities for shop_a and shop_b
+                for rent_a in rent_prob_a: # rent_a and rent_b are tuples -> (rent count, rent probability)
+                    for return_a in return_prob_a:
+                        for rent_b in rent_prob_b:
+                            for return_b in return_prob_b:
+                                # print(f'Rent prob: {rent_a[1]}, Return prob: {return_a[1]}')
+                                # print(f'Rent prob: {rent_b[1]}, Return prob: {return_b[1]}')
+                                # print(f'Policy: {state_array[i, j].policy}')
+                                # print(f'Value: {state_array[i, j].val}')
+                                state_new_value = calc_sigma(rent_a, state_array, rent_b, return_a, return_b, i, j)
+                                
+                state_array[i, j].val = state_new_value
+                delta = max(delta, abs(state_value_temp - state_array[i, j].val))
+                print(f'Delta: {delta}')
+
+
+
+
+        break
+
 
     return None
 
