2485092d994d3304655e2fbb1396a90f335c563e
Onur Sevket Aslan <osevas@gmail.com>
2024-03-19 23:55:08 +0300

    finished coding state space. coding 1 episode

--- chapter6/windy_gridworld.py
+++ chapter6/windy_gridworld.py
@@ -16,6 +16,7 @@ class State:
         else:
             self.value = 0 # initialization.  initial value is 0
         self.edges = []
+        self.action_val = {'up':0, 'down':0, 'right':0, 'left':0}
     
     def update_edges(self, edge: str) -> None:
         """
@@ -24,14 +25,14 @@ class State:
         """
         self.edges.append(edge)
     
-    def update_val(self, val:int) -> None:
+    def update_state_action_val(self, action:str , val:int) -> None:
         """
         updating state value
 
         Args:
             val (int): new value
         """
-        self.value = val
+        self.action_val[action] = val
 
 class Gridworld:
     """
@@ -49,25 +50,28 @@ class Gridworld:
             for j in range(n_grid_x):
                 if i == 0 and j == 0:
                     self.state_array[i, j].update_edges('left')
-                    self.state_array[i, j].update_edges('down')
-                elif i == 0 and j == n_grid_y - 1:
+                    self.state_array[i, j].update_edges('up')
+                elif i == 0 and j == n_grid_x - 1:
                     self.state_array[i, j].update_edges('right')
-                    self.state_array[i, j].update_edges('down')
+                    self.state_array[i, j].update_edges('up')
                 elif i == 0:
-                    self.state_array[i, j].update_edges('down')
-                elif i != 0 and i != n_grid_x - 1:
+                    self.state_array[i, j].update_edges('up')
+                elif i != 0 and i != n_grid_y - 1:
                     if j == 0:
                         self.state_array[i, j].update_edges('left')
-                    if j == n_grid_y - 1:
+                    if j == n_grid_x - 1:
                         self.state_array[i, j].update_edges('right')
-                elif i == n_grid_x - 1 and j == 0:
+                elif i == n_grid_y - 1 and j == 0:
                     self.state_array[i, j].update_edges('left')
-                    self.state_array[i, j].update_edges('up')
-                elif i == n_grid_x - 1 and j == n_grid_y - 1:
+                    self.state_array[i, j].update_edges('down')
+                elif i == n_grid_y - 1 and j == n_grid_x - 1:
                     self.state_array[i, j].update_edges('right')
-                    self.state_array[i, j].update_edges('up')
-                elif i == n_grid_x - 1:
-                    self.state_array[i, j].update_edges('up')
+                    self.state_array[i, j].update_edges('down')
+                elif i == n_grid_y - 1:
+                    self.state_array[i, j].update_edges('down')
+        
+        # assigning terminal states
+        self.state_array[3, 7].terminal = True
     
     def state_to_arr(self) -> np.ndarray:
         """
@@ -110,7 +114,25 @@ class Agent:
         actions = {'up':self.action_prob, 'down':self.action_prob, 'right':self.action_prob, 'left':self.action_prob}
         return actions
 
-def play(environment:object, agent:object, theta:float = 0.001, gamma:int = 1, max_iter:int = 50, reward:int = -1) -> None:
+def e_greedy(state:object, timestep:int) -> str:
+    """
+    function to select an action
+
+    Args:
+        state (object): _description_
+        timestep (int): _description_
+
+    Returns:
+        str: _description_
+    """
+    epsilon = 1/timestep
+    p = np.random.random()
+    if p < epsilon:
+        return np.random.choice(list(state.action_val.keys()))
+    return max(state.action_val, key = state.action_val.get)
+
+
+def play(environment:object, alpha:float = 0.001, gamma:int = 1, max_iter:int = 50, reward:int = -1) -> None:
     """
     function to start an iteration
 
@@ -122,50 +144,17 @@ def play(environment:object, agent:object, theta:float = 0.001, gamma:int = 1, m
         max_iter (int, optional): _description_. Defaults to 50.
         reward (int, optional): _description_. Defaults to -1.
     """
-    new_state_arr = np.zeros((environment.n_grid, environment.n_grid))
-    num_iter = 1
-    delta = 1
+    current_state = environment.state_array[3, 0]
 
-    while(delta > theta and num_iter <= max_iter):
+    for i in range(max_iter):
+        timestep = 1
+        while not current_state.terminal:
+            
+            act1 = e_greedy(current_state, timestep) # action1 of Sarsa
 
-        for i in range(environment.n_grid):
-            for j in range(environment.n_grid): # going through the grid
-                if not environment.state_array[i, j].terminal: # work on states that are not terminal
-                    allowable_actions = agent.move() # list allowable actions of the agent
-                    new_value = 0
-                    for action in allowable_actions.keys():
-                        if action in environment.state_array[i, j].edges: # checking if we hit edge wall or not.  if we hit, use state's current value
-                            value_action = allowable_actions[action] * (reward + gamma * environment.state_array[i, j].value)
-                            new_value += value_action
-                        else: # if we do not hit a wall
-                            if action == 'up':
-                                value_action = allowable_actions[action] * (reward + gamma * environment.state_array[i-1, j].value)
-                                new_value += value_action
-                            elif action == 'down':
-                                value_action = allowable_actions[action] * (reward + gamma * environment.state_array[i+1, j].value)
-                                new_value += value_action
-                            elif action == 'right':
-                                value_action = allowable_actions[action] * (reward + gamma * environment.state_array[i, j+1].value)
-                                new_value += value_action
-                            elif action == 'left':
-                                value_action = allowable_actions[action] * (reward + gamma * environment.state_array[i, j-1].value)
-                                new_value += value_action
-                    new_state_arr[i, j] = new_value
+            
         
-        # calculating delta
-        current_state_arr = environment.state_to_arr()
-        delta = np.amax(np.absolute(current_state_arr - new_state_arr))
 
-        print('\nCurrent iteration: {}'.format(num_iter))
-        print('\nCurrent state values:')
-        print(current_state_arr)
-
-        print('\nDelta: {}'.format(delta))
-
-        # updating state values
-        environment.arr_to_state(new_state_arr)
-
-        num_iter += 1
 
 
 
@@ -175,3 +164,4 @@ if __name__ == "__main__":
     # play(env, agn)
     state_space_values = env.state_to_arr()
     print(state_space_values)
+    print(env.state_array[0, 4].edges)
